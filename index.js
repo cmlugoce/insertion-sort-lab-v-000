02b0b6b4373648908fcb6dc99e7c61454210c7e3
Cristina Lugo <cmlugoce@syr.edu>
2018-10-29 16:38:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let min = array[0];
 function insertionSort(array){
  let newMin;
     let sorted = [];
-    while(array.length != 0) {
+    while(array.length !== 0) {
         newMin = findMinAndRemove(array);
         sorted.push(newMin);
     }
